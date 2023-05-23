09a66cd6d7b7b1271525ab388c7278178a267574
VinaySehwag14 <vinaysehwag14@gmail.com>
2023-05-23 13:03:44 +0530

    added styles and file structure

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import { Actors, MovieInfo, Movies, NavBar, Profile } from './';
+import useStyles from './styles';
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolkit} />
+
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/approved" element={<Movies />} />
+          <Route exact path="/movie/:id" element={<MovieInfo />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
